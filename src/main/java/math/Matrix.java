02e1d84a5cc54ac7623131de28e8b30469e340ef
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-01-16 23:01:59 +0100

    jørgen

--- src/main/java/math/Matrix.java
+++ src/main/java/math/Matrix.java
@@ -1,14 +1,63 @@
 package math;
 
+import java.util.Arrays;
+
 public class Matrix {
     private double[][] matrix;
+    private int width;
+    private int height;
     public Matrix(double[][] matrix){
         this.matrix = matrix;
+        width = matrix[0].length;
+        height = matrix.length;
     }
-    public Matrix(int width, int height){
+    public Matrix(int height, int width){
+        this.height = height;
+        this.width = width;
+        matrix = new double[height][width];
     }
 
-    public void clear(){
-
+    public Vector linearTransformation(Vector vector){
+        if(vector.getDimensions() != width)
+            throw new IllegalArgumentException("The vectors number of dimensions doesnt match the matrix width");
+        Vector newVec = new Vector(vector.getDimensions());
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                newVec.setElement(y, newVec.getElement(y) + vector.getElement(y) * matrix[y][x]);
+            }
+        }
+        return newVec;
     }
+
+
+    @Override
+    public String toString(){
+        String s = "[\n";
+        for(int y = 0; y < height; y++){
+            s += "[";
+            for(int x = 0; x < width; x++){
+                s += matrix[y][x];
+                if(x != width-1)
+                    s += ", ";
+            }
+            s+= "]\n";
+        }
+
+        s += "]";
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double[][] arr = {
+                {1, 2},
+                {2, 1}
+        };
+
+        Matrix m = new Matrix(arr);
+
+        System.out.println(m.linearTransformation(new Vector(2, 4)));
+        System.out.println(m);
+    }
+
 }
