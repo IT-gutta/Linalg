8fdd0781cdf32dc1e764bc5983dc89901994a1ea
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-01-21 16:09:02 +0100

    jørgen

--- src/main/java/graphics/toolbar/NewMenuButton.java
+++ src/main/java/graphics/toolbar/NewMenuButton.java
@@ -1,6 +1,7 @@
 package graphics.toolbar;
 
 import graphics.CanvasRenderer;
+import graphics.DefinedVariables;
 import javafx.event.ActionEvent;
 import javafx.event.EventDispatchChain;
 import javafx.event.EventHandler;
@@ -12,18 +13,23 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.PopupWindow;
 import javafx.stage.Window;
+import math.Vector;
 import org.linalgfx.App;
 
 import java.util.List;
 
 public class NewMenuButton extends MenuButton {
     private Popup popup = new Popup();
+    private TextInputDialog dialog = new TextInputDialog("name");
+    private Button mongoOppleggButton = new Button();
 
     public NewMenuButton() {
         super("Add New");
+        dialog.initModality(Modality.APPLICATION_MODAL);
 
         MenuItem vector = new MenuItem("2DVector");
         vector.setOnAction(actionEvent -> {
@@ -35,8 +41,10 @@ public class NewMenuButton extends MenuButton {
             TextField yInput = DoubleFormatter.getTextField();
 
             vectorInput.getChildren().addAll(new Text("Skriv inn x- og y-koordinater:   "), xInput, yInput);
-            popup.getContent().add(vectorInput);
-            popup.show(this, App.getWidth()/2, App.getWidth()/4);
+            dialog.setGraphic(vectorInput);
+            dialog.showAndWait().ifPresent(response ->{
+                DefinedVariables.add(new Vector((double) xInput.getTextFormatter().getValue(), (double) yInput.getTextFormatter().getValue()), dialog.getEditor().getText());
+            });
         });
 
         MenuItem matrix = new MenuItem("Matrix");
