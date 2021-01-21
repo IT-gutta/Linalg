d49827371d905fd7f4adca9111a01427a513b1c8
Halvor Linder Henriksen <halvorlh@stud.ntnu.no>
2021-01-21 10:56:31 +0100

    halvor vector

--- src/main/java/math/Mapping.java
+++ src/main/java/math/Mapping.java
@@ -1,22 +1,47 @@
 package math;
 
+import graphics.Renderable;
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.ArrayList;
 import java.util.function.Function;
 
-public class Mapping {
+public class Mapping implements Renderable {
     private final Function<Double,Double> mapping;
     private double start;
     private double end;
-    public Mapping(Function<Double, Double> fun, double start, double end){
+    private double step;
+    private ArrayList<Point> points;
+    public Mapping(Function<Double, Double> fun, double start, double end, double step){
         mapping = fun;
         this.start = start;
         this.end = end;
+        this.step = step;
+        initPoints();
     }
     public Mapping(Function<Double, Double> fun){
         mapping = fun;
         start = -10;
         end = 10;
+        step = Math.pow(10, -3);
+        initPoints();
+    }
+    public void initPoints(){
+        double d = start;
+        while(d<=end){
+            points.add(new Point(d, evaluate(d)));
+        }
     }
     public double evaluate(double x){
         return mapping.apply(x);
     }
+
+    public void render(GraphicsContext gc){
+        for(Point p:points){
+            Point q = p;
+        }
+    }
+
+
+
 }
