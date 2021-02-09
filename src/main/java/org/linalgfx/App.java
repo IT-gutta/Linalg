fc5a44f86e6e449d61f6ac860d4b60278a7b6d79
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2021-02-09 13:08:28 +0100

    graphicscxontext 3d

--- src/main/java/org/linalgfx/App.java
+++ src/main/java/org/linalgfx/App.java
@@ -12,13 +12,10 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.*;
-import java.util.List;
 
 /**
  * JavaFX App
